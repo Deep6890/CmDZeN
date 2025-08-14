f009b70a3c4c35b0f17eea2e00c1dacc77d3a8c6
Deep4054 <deepkayastha4054.com>
2025-08-15 01:25:42 +0530

    New Home page created and nav bar, website blocker added timer added

---
+++ ElectronZenGUI/CmDZeNGUI/src/components/LogoAndThings/blockLogo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function BlockLogo() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="#a855f7"
+            className="w-8 h-8"
+            fill="none"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+            />
+        </svg>
+    );
+}
--- ElectronZenGUI/CmDZeNGUI/src/components/PreBuildComponts/websiteBlocker.jsx
+++ ElectronZenGUI/CmDZeNGUI/src/components/PreBuildComponts/websiteBlocker.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import BlockLogo from '../LogoAndThings/blockLogo'
 export default function WebsiteBlocker() {
     return (
         <div className='w-4/5 flex justify-center flex-col'>
-            <div className='w-full flex justify-start' >
-
+            <div className='w-full flex justify-between items-center' >
+                <div className='w-2/3 flex gap-3'>
+                    <BlockLogo />
+                    <h1 className='text-2xl font-semibold'>Blocked Websites</h1>
+                </div>
+                <div className='w-1/3'>
+                </div>
             </div>
         </div>
     )
